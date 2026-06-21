6589d6f84f52ecbfc945a3f00ded34d105fbfdc9
Brian Kennedy <brian@grug.dev>
2026-06-20 23:51:36 -0400

    fixed(umami): Trying to fix double tracking

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -44,8 +44,12 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
             src: `${import.meta.env.VITE_UMAMI_WEBSITE_URL}/script.js`,
             defer: true,
             'data-website-id': import.meta.env.VITE_UMAMI_WEBSITE_ID,
-            'data-performance': import.meta.env.VITE_UMAMI_PERFORMANCE_TRACKING,
             'data-do-not-track': import.meta.env.VITE_UMAMI_DO_NOT_TRACK,
+          },
+          {
+            src: `${import.meta.env.VITE_UMAMI_WEBSITE_URL}/recorder.js`,
+            defer: true,
+            'data-website-id': import.meta.env.VITE_UMAMI_WEBSITE_ID,
             'data-sample-rate': '0.15',
             'data-mask-level': 'moderate',
             'data-max-duration': '300000',
